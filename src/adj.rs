2dcdaf262aa2fee304a3024ca29f390ec4e2f1f4
zombkit <stephen.korzen@gmail.com>
2025-03-02 19:01:07 -0800

    k

---
+++ src/adj.rs
@@ -0,0 +1,17 @@
+use crate::grammar::*;
+
+impl English {
+    pub fn adjective(word: &str, number: &Number) -> String {
+        match number {
+            Number::Singular => word.to_string(),
+            Number::Plural => match word {
+                "a" | "an" => "some".to_string(),
+                "this" => "these".to_string(),
+                "that" => "those".to_string(),
+                _ => word.to_string(),
+            },
+        }
+    }
+    //dog's -> dogs', child's -> children's, Mary's -> Marys'
+    //  pub fn genitive_adjective(word: &str, number: &Number) -> String {}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod adj;
 mod grammar;
 mod noun;
 mod utils;
