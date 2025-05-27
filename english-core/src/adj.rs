2fa415716edbf1dac915d74d6bc026ebd548bdf6
zombkit <stephen.korzen@gmail.com>
2025-05-27 02:25:47 -0700

    j

--- english-core/src/adj.rs
+++ english-core/src/adj.rs
@@ -12,6 +12,72 @@ impl EnglishCore {
             },
         }
     }
+    pub fn pronoun(person: &Person, number: &Number, gender: &Gender, case: &Case) -> &'static str {
+        match number {
+            Number::Singular => match person {
+                Person::First => match case {
+                    Case::Nominative => "I",
+                    Case::Accusative => "me",
+                    Case::Reflexive => "myself",
+                    Case::Possessive => "mine",
+                    Case::PersonalPossesive => "my",
+                },
+                Person::Second => match case {
+                    Case::Nominative => "you",
+                    Case::Accusative => "you",
+                    Case::Reflexive => "yourself",
+                    Case::Possessive => "yours",
+                    Case::PersonalPossesive => "your",
+                },
+                Person::Third => match gender {
+                    Gender::Masculine => match case {
+                        Case::Nominative => "he",
+                        Case::Accusative => "him",
+                        Case::Reflexive => "himself",
+                        Case::Possessive => "his",
+                        Case::PersonalPossesive => "his",
+                    },
+                    Gender::Feminine => match case {
+                        Case::Nominative => "she",
+                        Case::Accusative => "her",
+                        Case::Reflexive => "herself",
+                        Case::Possessive => "hers",
+                        Case::PersonalPossesive => "her",
+                    },
+                    Gender::Neuter => match case {
+                        Case::Nominative => "it",
+                        Case::Accusative => "it",
+                        Case::Reflexive => "itself",
+                        Case::Possessive => "its",
+                        Case::PersonalPossesive => "its",
+                    },
+                },
+            },
+            Number::Plural => match person {
+                Person::First => match case {
+                    Case::Nominative => "we",
+                    Case::Accusative => "us",
+                    Case::Reflexive => "ourselves",
+                    Case::Possessive => "ours",
+                    Case::PersonalPossesive => "our",
+                },
+                Person::Second => match case {
+                    Case::Nominative => "you",
+                    Case::Accusative => "you",
+                    Case::Reflexive => "yourselves",
+                    Case::Possessive => "yours",
+                    Case::PersonalPossesive => "your",
+                },
+                Person::Third => match case {
+                    Case::Nominative => "they",
+                    Case::Accusative => "them",
+                    Case::Reflexive => "themselves",
+                    Case::Possessive => "theirs",
+                    Case::PersonalPossesive => "their",
+                },
+            },
+        }
+    }
     //dog's -> dogs', child's -> children's, Mary's -> Marys'
     //  pub fn genitive_adjective(word: &str, number: &Number) -> String {}
 }
--- english-core/src/noun.rs
+++ english-core/src/noun.rs
@@ -1,5 +1,30 @@
 use crate::grammar::*;
 
+impl EnglishCore {
+    pub fn noun(word: &str, number: &Number) -> String {
+        match number {
+            Number::Singular => return word.to_string(),
+            Number::Plural => return EnglishCore::pluralize_noun(word),
+        }
+    }
+
+    fn irregular_suffix(word: &str) -> Option<String> {
+        for (sing, plur) in IRREGULAR_SUFFIXES {
+            if word.ends_with(sing) {
+                return Some(EnglishCore::replace_last_occurence(word, sing, plur));
+            }
+        }
+        None
+    }
+
+    pub fn pluralize_noun(word: &str) -> String {
+        if let Some(irr) = EnglishCore::irregular_suffix(word) {
+            return irr;
+        }
+        format!("{}{}", word, "s")
+    }
+}
+
 //These are most of the irregular suffixes, not counted so far are wolves,potatoes,compound words
 const IRREGULAR_SUFFIXES: &[(&str, &str)] = &[
     ("fish", "fish"),
@@ -31,94 +56,3 @@ const IRREGULAR_SUFFIXES: &[(&str, &str)] = &[
     ("sh", "shes"),
     ("ss", "sses"),
 ];
-
-impl EnglishCore {
-    pub fn noun(word: &str, number: &Number) -> String {
-        match number {
-            Number::Singular => return word.to_string(),
-            Number::Plural => return EnglishCore::pluralize_noun(word),
-        }
-    }
-    pub fn pronoun(person: &Person, number: &Number, gender: &Gender, case: &Case) -> &'static str {
-        match number {
-            Number::Singular => match person {
-                Person::First => match case {
-                    Case::Nominative => "I",
-                    Case::Accusative => "me",
-                    Case::Reflexive => "myself",
-                    Case::Possessive => "mine",
-                    Case::PersonalPossesive => "my",
-                },
-                Person::Second => match case {
-                    Case::Nominative => "you",
-                    Case::Accusative => "you",
-                    Case::Reflexive => "yourself",
-                    Case::Possessive => "yours",
-                    Case::PersonalPossesive => "your",
-                },
-                Person::Third => match gender {
-                    Gender::Masculine => match case {
-                        Case::Nominative => "he",
-                        Case::Accusative => "him",
-                        Case::Reflexive => "himself",
-                        Case::Possessive => "his",
-                        Case::PersonalPossesive => "his",
-                    },
-                    Gender::Feminine => match case {
-                        Case::Nominative => "she",
-                        Case::Accusative => "her",
-                        Case::Reflexive => "herself",
-                        Case::Possessive => "hers",
-                        Case::PersonalPossesive => "her",
-                    },
-                    Gender::Neuter => match case {
-                        Case::Nominative => "it",
-                        Case::Accusative => "it",
-                        Case::Reflexive => "itself",
-                        Case::Possessive => "its",
-                        Case::PersonalPossesive => "its",
-                    },
-                },
-            },
-            Number::Plural => match person {
-                Person::First => match case {
-                    Case::Nominative => "we",
-                    Case::Accusative => "us",
-                    Case::Reflexive => "ourselves",
-                    Case::Possessive => "ours",
-                    Case::PersonalPossesive => "our",
-                },
-                Person::Second => match case {
-                    Case::Nominative => "you",
-                    Case::Accusative => "you",
-                    Case::Reflexive => "yourselves",
-                    Case::Possessive => "yours",
-                    Case::PersonalPossesive => "your",
-                },
-                Person::Third => match case {
-                    Case::Nominative => "they",
-                    Case::Accusative => "them",
-                    Case::Reflexive => "themselves",
-                    Case::Possessive => "theirs",
-                    Case::PersonalPossesive => "their",
-                },
-            },
-        }
-    }
-
-    fn irregular_suffix(word: &str) -> Option<String> {
-        for (sing, plur) in IRREGULAR_SUFFIXES {
-            if word.ends_with(sing) {
-                return Some(EnglishCore::replace_last_occurence(word, sing, plur));
-            }
-        }
-        None
-    }
-
-    pub fn pluralize_noun(word: &str) -> String {
-        if let Some(irr) = EnglishCore::irregular_suffix(word) {
-            return irr;
-        }
-        format!("{}{}", word, "s")
-    }
-}
