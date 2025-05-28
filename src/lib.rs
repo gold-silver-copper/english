694970dca977ddc1ec4c85503454e4062f9516a9
zombkit <stephen.korzen@gmail.com>
2025-05-27 17:59:06 -0700

    s

--- src/lib.rs
+++ src/lib.rs
@@ -20,4 +20,31 @@ impl English {
             }
         }
     }
+    pub fn verb(
+        word: &str,
+        person: &Person,
+        number: &Number,
+        tense: &Tense,
+        form: &Form,
+    ) -> String {
+        match get_verb_forms(word) {
+            Some(wordik) => match (person, number, tense, form) {
+                (_, _, _, Form::Infinitive) => {
+                    return word.to_string();
+                }
+
+                (Person::Third, Number::Singular, Tense::Present, Form::Finite) => {
+                    wordik.0.to_string()
+                }
+                (_, _, Tense::Present, Form::Finite) => {
+                    return word.to_string();
+                }
+                (_, _, Tense::Present, Form::Participle) => wordik.2.to_string(),
+                (_, _, Tense::Past, Form::Participle) => wordik.3.to_string(),
+
+                (_, _, Tense::Past, Form::Finite) => wordik.1.to_string(),
+            },
+            None => EnglishCore::verb(word, person, number, tense, form),
+        }
+    }
 }
