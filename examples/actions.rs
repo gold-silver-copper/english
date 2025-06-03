74c52e470ab68d7801d91313c970b6b843f1a952
zombkit <stephen.korzen@gmail.com>
2025-06-02 23:16:39 -0700

    d

--- examples/actions.rs
+++ examples/actions.rs
@@ -69,3 +69,160 @@ fn main() {
 
     println!("{}", English::noun("thyridium", &Number::Plural));
 }
+
+pub struct Human {
+    name: String,
+    eye_color: EyeColor,
+    hair_color: HairColor,
+    skin_color: SkinColor,
+    height: usize,
+    weight: usize,
+    age: usize,
+    gender: Gender,
+    location: Locative,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HairStyle {
+    Bald,
+    Short,
+    Medium,
+    Long,
+    Ponytail,
+    Bun,
+    Braided,
+    Mohawk,
+    Curly,
+    Wavy,
+    Straight,
+    Dreadlocks,
+    Tangled,
+    Spiky,
+    Undercut,
+    Messy,
+    Tiedback,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum FacialHair {
+    None,
+    Mustache,
+    Beard,
+    Goatee,
+    Sideburns,
+    Stubble,
+    FullBeard,
+    CleanShaven,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum EyeShape {
+    Almond,
+    Round,
+    Hooded,
+    Monolid,
+    Downturned,
+    Upturned,
+    Elven,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum NoseType {
+    Button,
+    Roman,
+    Straight,
+    Nubian,
+    Hawk,
+    Snub,
+    Aquiline,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum LipShape {
+    Thin,
+    Full,
+    HeartShaped,
+    Round,
+    BowShaped,
+    Small,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum EarType {
+    Small,
+    Large,
+    Pointed,
+    Round,
+    AttachedLobe,
+    DetachedLobe,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Jawline {
+    Soft,
+    Sharp,
+    Square,
+    Round,
+    Oval,
+    ProminentChin,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Freckles {
+    None,
+    Few,
+    Moderate,
+    Many,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Scars {
+    None,
+    Small,
+    Noticeable,
+    Prominent,
+    AcrossEye,
+    Burn,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Tattoo {
+    Geometric,
+    Animal(AnimalType),
+    Script(String),
+}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum AnimalType {
+    Wolf,
+    Fox,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HairColor {
+    Blonde,
+    Brown,
+    Black,
+    Red,
+    Gray,
+    White,
+    Bald,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum SkinColor {
+    Pale,
+    Fair,
+    Olive,
+    Sunkissed,
+    Tan,
+}
+
+pub enum EyeColor {
+    Blue,
+    Green,
+    Gray,
+    Hazel,
+    Brown,
+    Amber,
+    Violet,
+}
