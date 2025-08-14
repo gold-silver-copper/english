782324c016035872560f1a01b4a81f35ba12863b
zombkit <stephen.korzen@gmail.com>
2025-08-13 19:26:31 -0700

    nouns

--- extractor/src/main.rs
+++ extractor/src/main.rs
@@ -60,13 +60,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input_path = "../../english.jsonl";
 
     extract_verb_conjugations_new(input_path, "verb_conjugations.csv")?;
-
-    /*  extract_irregular_nouns(input_path, "nouns_with_plurals.csv")?;
+    extract_irregular_nouns(input_path, "nouns_with_plurals.csv")?;
 
     extract_irregular_adjectives(input_path, "adjectives.csv")?;
     generate_adjectives_file("adjectives.csv", "adjiki.rs");
     generate_nouns_file("nouns_with_plurals.csv", "nounsiki.rs");
-    generate_verbs_file("verb_conjugations.csv", "verbsiki.rs");  */
+    generate_verbs_file("verb_conjugations.csv", "verbsiki.rs");
+
+    /*   */
     Ok(())
 }
 
