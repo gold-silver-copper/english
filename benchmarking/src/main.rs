51ca5f5a58a1b57ace50d2b590f6a22e26589637
zombkit <stephen.korzen@gmail.com>
2025-08-14 17:25:47 -0700

    benchmark

---
+++ benchmarking/src/main.rs
@@ -0,0 +1,11 @@
+use csv::Writer;
+use english::*;
+use serde::Deserialize;
+use std::collections::{HashMap, HashSet};
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+
+fn main() {
+    println!("Hello, world!");
+}
