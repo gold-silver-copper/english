768e4c843b3cdc181e5a1617a4c84a01a5df9f72
zombkit <stephen.korzen@gmail.com>
2025-08-17 22:58:24 -0700

    Revert "step 1 of deleting old noun impl"
    This reverts commit 861af0a0fe5ec47b6fb11e48f1181282db8055df.

--- examples/test.rs
+++ examples/test.rs
@@ -1,23 +1,23 @@
 use english::*;
 fn main() {
-    /* // --- Nouns ---
-      // Regular plurals
-      assert_eq!(English::noun("cat", &Number::Plural), "cats");
+    // --- Nouns ---
+    // Regular plurals
+    assert_eq!(English::noun("cat", &Number::Plural), "cats");
 
-      // Irregular plurals
-      // Add a number 2-9 to the end of the word to try different forms.
-      assert_eq!(English::noun("child", &Number::Plural), "children");
-      assert_eq!(English::noun("die", &Number::Plural), "dies");
-      assert_eq!(English::noun("die2", &Number::Plural), "dice");
+    // Irregular plurals
+    // Add a number 2-9 to the end of the word to try different forms.
+    assert_eq!(English::noun("child", &Number::Plural), "children");
+    assert_eq!(English::noun("die", &Number::Plural), "dies");
+    assert_eq!(English::noun("die2", &Number::Plural), "dice");
 
-      // Use count function for better ergonomics if needed
-      assert_eq!(English::count("man", 2), "men");
-      // Use count_with_number function to preserve the number
-      assert_eq!(English::count_with_number("nickel", 3), "3 nickels");
+    // Use count function for better ergonomics if needed
+    assert_eq!(English::count("man", 2), "men");
+    // Use count_with_number function to preserve the number
+    assert_eq!(English::count_with_number("nickel", 3), "3 nickels");
 
-      // Invariant nouns
-      assert_eq!(English::noun("sheep", &Number::Plural), "sheep");
-    */
+    // Invariant nouns
+    assert_eq!(English::noun("sheep", &Number::Plural), "sheep");
+
     // Complex nouns, note that From<&str> is impl'd for Noun
     // Note that noun(), count(), etc can work on both strings and Noun struct
     let jeans = Noun::from("pair").with_complement("of jeans");
@@ -130,7 +130,7 @@ fn main() {
     assert_eq!(English::add_possessive("dog"), "dog's");
     assert_eq!(English::add_possessive("dogs"), "dogs'");
 
-    /*   // --- Mixed Sentence Example ---
+    // --- Mixed Sentence Example ---
     let subject = English::noun("child", &Number::Plural);
     let verb = English::verb(
         "play",
@@ -139,8 +139,8 @@ fn main() {
         &Tense::Past,
         &Form::Finite,
     );
-    let object = English::noun("die2", &Number::Plural); */
+    let object = English::noun("die2", &Number::Plural);
 
-    //  let sentence = format!("The {} {} with {}.", subject, verb, object);
-    //assert_eq!(sentence, "The children played with dice.");
+    let sentence = format!("The {} {} with {}.", subject, verb, object);
+    assert_eq!(sentence, "The children played with dice.");
 }
