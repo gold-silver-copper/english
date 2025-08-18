f3fa717eae0acfa78677e3d29141b5e3f9f4509d
zombkit <stephen.korzen@gmail.com>
2025-08-17 21:32:23 -0700

    begin destruction of old noun impl

--- examples/speedmark.rs
+++ examples/speedmark.rs
@@ -5,6 +5,7 @@ fn main() {
     benchmark_verb();
     benchmark_noun();
     benchmark_adj();
+    benchmark_insane();
 }
 use std::hint::black_box;
 use std::time::Instant;
@@ -25,6 +26,13 @@ pub fn benchmark_noun() {
     let words = ["cat", "child", "mouse", "bus", "sheep", "fish"];
 
     run_benchmark("noun", &words, |w| English::noun(w, &Number::Plural));
+}
+pub fn benchmark_insane() {
+    let words = ["cat", "child", "mouse", "bus", "sheep", "fish"];
+
+    run_benchmark("insane", &words, |w| {
+        English::insane_noun(w, &Number::Plural)
+    });
 }
 
 pub fn benchmark_adj() {
