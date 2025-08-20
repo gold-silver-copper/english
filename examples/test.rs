45ebf074e529a68c0a353fc4f4ce0dc7b92ca610
zombkit <stephen.korzen@gmail.com>
2025-08-20 01:50:50 -0700

    neat

--- examples/test.rs
+++ examples/test.rs
@@ -1,66 +1,49 @@
 use english::*;
 fn main() {
+    // --- Mixed Sentence Example ---
+    let subject_number = Number::Plural;
+    let run = English::present_participle("run"); // running
+    let child = Noun::from("child").with_specifier(run); //running child
+    let subject = English::noun(child, &subject_number); //running children
+    let verb = English::verb(
+        "steal",
+        &Person::Third,
+        &subject_number,
+        &Tense::Past,
+        &Form::Finite,
+    ); //stole
+    let object = English::count_with_number("potato", 7); //7 potatoes
+
+    let sentence = format!("The {} {} {}.", subject, verb, object);
+    assert_eq!(sentence, "The running children stole 7 potatoes.");
+
     // --- Nouns ---
+    // Note that noun(), count(), etc can work on both strings and Noun struct
+    let jeans = Noun::from("pair").with_complement("of jeans");
+    assert_eq!(English::count_with_number(jeans, 3), "3 pairs of jeans");
     // Regular plurals
     assert_eq!(English::noun("cat", &Number::Plural), "cats");
-
-    // Irregular plurals
     // Add a number 2-9 to the end of the word to try different forms.
-    assert_eq!(English::noun("child", &Number::Plural), "children");
-    assert_eq!(English::noun("die", &Number::Plural), "dies");
-    assert_eq!(English::noun("die2", &Number::Plural), "dice");
-
+    // Can use plural()
+    assert_eq!(English::plural("die2"), "dice");
     // Use count function for better ergonomics if needed
     assert_eq!(English::count("man", 2), "men");
     // Use count_with_number function to preserve the number
     assert_eq!(English::count_with_number("nickel", 3), "3 nickels");
-
     // Invariant nouns
     assert_eq!(English::noun("sheep", &Number::Plural), "sheep");
 
-    // Complex nouns, note that From<&str> is impl'd for Noun
-    // Note that noun(), count(), etc can work on both strings and Noun struct
-    let jeans = Noun::from("pair").with_complement("of jeans");
-    assert_eq!(English::count_with_number(jeans, 3), "3 pairs of jeans");
-
-    // --- Adjectives ---
-    // Add a number 2-9 to the end of the word to try different forms. (Bad has the most forms at 3)
-    assert_eq!(English::adj("bad", &Degree::Comparative), "more bad");
-    assert_eq!(English::adj("bad", &Degree::Superlative), "most bad");
-    assert_eq!(English::adj("bad2", &Degree::Comparative), "badder");
-    assert_eq!(English::adj("bad2", &Degree::Superlative), "baddest");
-    assert_eq!(English::adj("bad3", &Degree::Comparative), "worse");
-    assert_eq!(English::adj("bad3", &Degree::Superlative), "worst");
-
-    // Complex verbs, note that From<&str> is impl'd for Verb
-    let pick_up = Verb::from("pick").with_particle("up");
-
-    // Past tense, third person singular
-    assert_eq!(
-        English::verb(
-            &pick_up,
-            &Person::Third,
-            &Number::Singular,
-            &Tense::Past,
-            &Form::Finite
-        ),
-        "picked up"
-    );
-
     // --- Verbs ---
-    // Regular verbs, note that verb() can be used on both strings and Verb struct
-    assert_eq!(
-        English::verb(
-            "walk",
-            &Person::First,
-            &Number::Singular,
-            &Tense::Present,
-            &Form::Participle
-        ),
-        "walking"
-    );
-
-    // Irregular verbs
+    // All verb functions can use either strings or Verb struct
+    let pick_up = Verb::from("pick").with_particle("up");
+    // Helper functions: past() , third_person(), present_participle(), infinitive() etc.
+    assert_eq!(English::past(&pick_up,), "picked up");
+    assert_eq!(English::present_participle("walk"), "walking");
+    assert_eq!(English::past_participle("go"), "gone");
+    // Add a number 2-9 to the end of the word to try different forms.
+    assert_eq!(English::past("lie"), "lay");
+    assert_eq!(English::past("lie2"), "lied");
+    // "to be" has the most verb forms in english and requires using verb()
     assert_eq!(
         English::verb(
             "be",
@@ -71,36 +54,16 @@ fn main() {
         ),
         "am"
     );
-    assert_eq!(
-        English::verb(
-            "go",
-            &Person::Third,
-            &Number::Plural,
-            &Tense::Past,
-            &Form::Participle
-        ),
-        "gone"
-    );
-    assert_eq!(
-        English::verb(
-            "lie",
-            &Person::First,
-            &Number::Singular,
-            &Tense::Past,
-            &Form::Finite
-        ),
-        "lay"
-    );
-    assert_eq!(
-        English::verb(
-            "lie2",
-            &Person::First,
-            &Number::Singular,
-            &Tense::Past,
-            &Form::Finite
-        ),
-        "lied"
-    );
+
+    // --- Adjectives ---
+    // Add a number 2-9 to the end of the word to try different forms. (Bad has the most forms at 3)
+    assert_eq!(English::adj("bad", &Degree::Comparative), "more bad");
+    assert_eq!(English::adj("bad", &Degree::Superlative), "most bad");
+    assert_eq!(English::comparative("bad2"), "badder");
+    assert_eq!(English::superlative("bad2"), "baddest");
+    assert_eq!(English::comparative("bad3"), "worse");
+    assert_eq!(English::superlative("bad3"), "worst");
+    assert_eq!(English::positive("bad3"), "bad");
 
     // --- Pronouns ---
     assert_eq!(
@@ -125,19 +88,4 @@ fn main() {
     // --- Possessives ---
     assert_eq!(English::add_possessive("dog"), "dog's");
     assert_eq!(English::add_possessive("dogs"), "dogs'");
-
-    // --- Mixed Sentence Example ---
-    let subject_number = Number::Plural;
-    let subject = English::noun("child", &subject_number);
-    let verb = English::verb(
-        "play",
-        &Person::Third,
-        &subject_number,
-        &Tense::Past,
-        &Form::Finite,
-    );
-    let object = English::noun("die2", &Number::Plural);
-
-    let sentence = format!("The {} {} with {}.", subject, verb, object);
-    assert_eq!(sentence, "The children played with dice.");
 }
